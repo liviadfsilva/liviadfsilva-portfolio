89fb9e1a0d3b230afcadf7472c46dfd68b6217a5
liviadfsilva <liviadfsilva@gmail.com>
2026-06-24 13:45:22 -0300

    refactor: refactor translations

--- src/app/lib/i18n.ts
+++ src/app/lib/i18n.ts
@@ -28,22 +28,29 @@ export const translations: Record<Language, Translation> = {
         role: "Desenvolvedora Backend",
         whoAmI: "Quem sou eu?",
         about:
-            "Sou Desenvolvedora de Software e graduanda em Ciência da Computação, apaixonada por construir sistemas backend. Gosto muito de me desafiar aprendendo como as coisas funcionam e colocando meu conhecimento em prática. Me desafiar de formas criativas é o que faço de melhor! Meu objetivo número 1 é me tornar excelente no que faço, sem nunca esquecer que estou em constante evolução.",
+            "Sou Desenvolvedora e graduanda em Ciência da Computação, apaixonada por construir sistemas backend. " +
+            "Gosto muito de me desafiar através do aprendizado e colocando meu conhecimento em prática. " +
+            "Desafiar-me de formas criativas é o que faço de melhor! Meu principal objetivo é alcançar excelência no " +
+            "que eu faço, sem nunca esquecer que estou em constante evolução.",
         connect: "Vamos nos conectar!",
-        findMe: "Você pode me encontrar em:",
+        findMe: "Onde me encontrar:",
         skills: "Habilidades",
         myProjects: "Meus Projetos",
         goToRepo: "Ver repositório",
         builtWithLove: "Transformando ideias em soluções.",
         projects: {
             "book-review-blog":
-                "Uma API RESTful para gerenciar usuários, livros, resenhas e posts pessoais de blog, projetada para sustentar uma plataforma completa de resenhas e blog sobre livros.",
+                "Plataforma para publicação de de resenha de livros projetada para gerenciar usuários, livros, " +
+                "resenhas e posts pessoais de blog.",
             "pixel-peel":
-                "Uma API RESTful criada para sustentar uma plataforma de e-commerce de adesivos digitais, gerenciando produtos, usuários, administradores, carrinho e pedidos.",
+                "Sistema desenvolvido para sustentar uma plataforma de e-commerce de adesivos digitais, " +
+                "gerenciando produtos, usuários, admins, carrinho e pedidos.",
             "task-matrix":
-                "Uma API profundamente inspirada na Matriz de Eisenhower, uma ferramenta focada em organizar e priorizar tarefas por importância e urgência.",
+                "Aplicação de gerenciamento de tarefas baseada na Matriz de Eisenhower para organização " +
+                "por importância e urgência.",
             "vet-clinic":
-                "Uma API RESTful para o gerenciamento de uma clínica veterinária, lidando com veterinários, usuários, pets, donos e consultas.",
+                "Sistema para gestão de clínicas veterinárias, incluindo veterinários, usuários, " +
+                "pets, donos e consultas.",
         },
     },
     "en-UK": {
@@ -54,7 +61,10 @@ export const translations: Record<Language, Translation> = {
         role: "Backend Developer",
         whoAmI: "Who am I?",
         about:
-            "I'm a Software Developer and Computer Science Major passionate about building backend systems. I'm very fond of challenging myself by learning how things work and putting my knowledge into practise. Challenging myself in creative ways is what I do best! My #1 goal is to become excellent at what I do while remaining aware that I'm a constant work in progress.",
+            "I'm a Software Developer and Computer Science Major passionate about building backend systems. " +
+            "I'm very fond of challenging myself by learning how things work and putting my knowledge into practise. " +
+            "Challenging myself in creative ways is what I do best! " +
+            "My #1 goal is to become excellent at what I do while remaining aware that I'm a constant work in progress.",
         connect: "Let's connect!",
         findMe: "You can find me on:",
         skills: "Skills",
@@ -63,13 +73,16 @@ export const translations: Record<Language, Translation> = {
         builtWithLove: "Turning ideas into meaningful solutions.",
         projects: {
             "book-review-blog":
-                "A RESTful API for managing users, books, reviews and personal blog posts, designed to support a complete book review and blogging platform.",
+                "An application designed to support a complete book review and blogging platform, " +
+                "managing users, books, reviews and personal blog posts.",
             "pixel-peel":
-                "A RESTful API built to support a digital stickers e-commerce platform, handling products, users, admins, cart and orders.",
+                "A system built to support a digital stickers e-commerce platform, handling products, users, " +
+                "admins, cart and orders.",
             "task-matrix":
-                "An API deeply inspired by the Eisenhower Matrix, a tool which focuses on organising and prioritising tasks by importance and urgency.",
+                "A task manager deeply inspired by the Eisenhower Matrix, a tool which focuses on organising and prioritising " +
+                "tasks by importance and urgency.",
             "vet-clinic":
-                "A RESTful API for the management of a veterinarian clinic, handling vets, users, pets, owners and appointments.",
+                "A management system for handling vets, users, pets, owners and appointments in a veterinary clinic.",
         },
     },
 }
